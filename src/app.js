c1b65f9b9f419894933193c6c66bce24b9d3d188
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-03-21 09:35:47 -0300

    :hammer: init part 2
    change, remove and insert scripts and documentation

---
+++ src/app.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import cors from 'cors';
+import { uuid } from 'uuidv4';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+let products = [];
+
+app.get('/products', (request, response) => {
+  // TODO: Listagem de todos os produtos.
+});
+
+app.post('/products', (request, response) => {
+  // TODO: Salva produto no array products.
+});
+
+app.put('/products/:id', (request, response) => {
+  // TODO: Atualiza produto por ID.
+});
+
+app.delete('/products/:code', (request, response) => {
+  // TODO: Remove TODOS os produtos que possuem o código passado por parâmetro.
+});
+
+app.post('/products/:code/love', (request, response) => {
+  // TODO: Incrementa em 1 o número de lovers de todos os produtos que possuam o code do parâmetro.
+});
+
+app.get('/products/:code', (request, response) => {
+  // TODO: Busca de todos os produtos com o código recebido por parâmetro.
+});
+
+export default app;
--- test/product.test.js
+++ test/product.test.js
@@ -1,18 +1,5 @@
-/**
- * A quantidade vendida pode ser de 1 ou mais unidades.
- */
+import request from 'supertest';
 
-import Product from '../src/model/product';
-import sell from '../src/service/sellProduct';
+import app from '../src/app';
 
-it('deve validar baixa de estoque da venda de uma unidade.', () => {
-  const product1 = new Product('Celular', 500.00, 900.00, 10);
-  sell(product1, 1);
-  expect(product1.stock).toBe(9);
-});
-
-it('deve validar baixa de estoque da venda de mais de uma unidade.', () => {
-  const product1 = new Product('Celular', 500.00, 900.00, 10);
-  sell(product1, 2);
-  expect(product1.stock).toBe(8);
-});
+let products;
